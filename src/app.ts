37e4d22333b83874e3a2594a890855ded30a05c4
Ash Mackay <ash.mackay85@gmail.com>
2022-05-03 11:28:39 +0100

    Setting Up a Google API Key

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
+const GOOGLE_API_KEY = 'AIzaSyCHvYxV7g6i-D-71cRbt15xJe0YkgOYl1M'; // Will be removed post-project
+
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
